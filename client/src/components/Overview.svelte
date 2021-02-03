dd08804c2f5ec58d843e1419d4e0fa10171152e6
Richard <rwebb2305@gmail.com>
2021-02-03 22:07:11 +0100

    feat: add splash screen for page start with explanation

---
+++ client/src/components/Overview.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { v4 } from "uuid";
+  import type { BurnerConfig } from "../types";
+
+  import Burner from "./Burner.svelte";
+  import Sidebar from "./Sidebar.svelte";
+  import Topbar from "./Topbar.svelte";
+
+  let history: Array<BurnerConfig> = [
+    { id: v4(), exercise: 1, sleep: 7, diet: 5, social: 9, date: new Date() },
+    { id: v4(), exercise: 3, sleep: 5, diet: 4, social: 8, date: new Date() },
+  ];
+
+  let activeBurners = getRandomHistory();
+
+  function getRandomHistory(): BurnerConfig {
+    return {
+      id: v4(),
+      exercise: getRandomScore(),
+      sleep: getRandomScore(),
+      diet: getRandomScore(),
+      social: getRandomScore(),
+      date: new Date(),
+    };
+  }
+
+  function getRandomScore() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  function saveToHistory() {
+    const existingItemIndex = history.findIndex(
+      ({ id }) => id === activeBurners.id
+    );
+    if (existingItemIndex > -1) {
+      history.splice(existingItemIndex, 1);
+    }
+    history = [...history, activeBurners];
+    activeBurners = getRandomHistory();
+  }
+</script>
+
+<main transition:fade={{ duration: 2000 }}>
+  <div class="grid">
+    <div class="sidebar">
+      <Sidebar
+        {history}
+        on:load-burners={({ detail: loadedBurners }) =>
+          (activeBurners = loadedBurners)}
+      />
+    </div>
+    <div class="topbar">
+      <Topbar on:save={saveToHistory} date={activeBurners.date} />
+    </div>
+    <div class="burner1" transition:fade={{ duration: 1000 }}>
+      <Burner bind:burner={activeBurners.exercise} burnerType="Exercise" />
+    </div>
+    <div class="burner2" transition:fade={{ duration: 1000, delay: 500 }}>
+      <Burner bind:burner={activeBurners.sleep} burnerType="Sleep" />
+    </div>
+    <div class="burner3" transition:fade={{ duration: 1000, delay: 1000 }}>
+      <Burner bind:burner={activeBurners.social} burnerType="Social" />
+    </div>
+    <div class="burner4" transition:fade={{ duration: 1000, delay: 1500 }}>
+      <Burner bind:burner={activeBurners.diet} burnerType="Diet" />
+    </div>
+    <div class="stats" />
+  </div>
+</main>
+
+<style>
+  .grid {
+    width: 100%;
+    height: 100%;
+    max-width: 800px;
+    max-height: 800px;
+    display: grid;
+    place-items: center;
+    grid-template-columns: 90px auto;
+    grid-template-rows: 40px auto auto 60px;
+    column-gap: 5%;
+    row-gap: 5%;
+  }
+
+  .topbar {
+    grid-column: 1 / span 3;
+    grid-row: 1;
+    align-self: center;
+    justify-self: flex-end;
+  }
+
+  .sidebar {
+    grid-column: 1;
+    grid-row: 2 / span 2;
+  }
+
+  .burner1 {
+    grid-column: 2;
+    grid-row: 2;
+  }
+  .burner2 {
+    grid-column: 3;
+    grid-row: 2;
+  }
+  .burner3 {
+    grid-column: 2;
+    grid-row: 3;
+  }
+  .burner4 {
+    grid-column: 3;
+    grid-row: 3;
+  }
+  .stats {
+    grid-column: 1 / span 3;
+    grid-row: 4;
+  }
+</style>
---
+++ client/src/components/Splash.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let isAcknowledged = false;
+  onMount(() => {
+    isAcknowledged = true;
+  });
+</script>
+
+<main>
+  <h1>Mental health</h1>
+  <p>lorem ipsum</p>
+  <h2>The burners</h2>
+  <p>lorem ipsum</p>
+  {#if isAcknowledged}
+    <h2>This app</h2>
+    <p>lorem ipsum</p>
+  {/if}
+</main>
+
+<style>
+</style>
